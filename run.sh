9a15d6605932e27d048d7c1e4dc5b5d4d8648401
HeYQ <hyqitjavaee@163.com>
2023-09-28 19:29:47 +0800

    更改run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 python main.py \
---bert_dir="../model_hub/bert-base-chinese/" \
+--bert_dir="./model_hub/bert_base_chinese/" \
 --data_dir="./data/final_data/" \
 --log_dir="./logs/" \
 --output_dir="./checkpoints/" \
---num_tags=65 \
+--num_tags=6 \
 --seed=123 \
 --gpu_ids="0" \
 --max_seq_len=128 \
 --lr=3e-5 \
 --other_lr=3e-4 \
---train_batch_size=32 \
---train_epochs=5 \
---eval_batch_size=32 \
+--train_batch_size=16 \
+--train_epochs=10 \
+--eval_batch_size=16\
